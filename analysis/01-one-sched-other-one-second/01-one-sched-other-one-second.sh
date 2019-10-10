45593fd66a317a145dd90ed44ce26b968da5a557
Hagen Paul Pfeifer <hagen@jauu.net>
2019-10-10 22:26:58 +0200

    add more scripts
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

---
+++ analysis/01-one-sched-other-one-second/01-one-sched-other-one-second.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+# this test calls ../runner with no argument for sched_deadline, 
+# so it will run with sched other, and it will run for a ~second
+# and will sleep for a second, and so on.
+
+# This simple non realine run is just an ninimal example to
+# call perf record, followed by perf script run to analyse the
+# output and print the waittime, runtime etc.
+# This analyzer scripts are used by the other scripts, this is
+# just an simple example to get things done.
+
+../src/runner ... &
+PID1=$!
+
+perf record -p $PID1
+PID_PERF=$!
+
+sleep 10
+
+kill $PID1
+kill $PID_PERF
