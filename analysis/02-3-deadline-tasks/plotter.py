2043c312e01ce0e862e9af225706b336e197ac55
Philipp Stanner <philipp.stanner@rohde-schwarz.com>
2020-04-15 15:47:04 +0200

    plotter dead: automated interrptd. h-bars

--- analysis/02-3-deadline-tasks/plotter.py
+++ analysis/02-3-deadline-tasks/plotter.py
@@ -15,16 +15,15 @@ for line in reader:
     runtimes.append(int(line[3]))
     start_time_current += int(line[2])
 
-bars = [(start, runtime) for start, runtime in zip(start_times, runtimes)]
-print(bars)
+bars = [(start, runtime) for start, runtime in zip(start_times[:100], runtimes[:100])]
 
 ax.broken_barh(bars, (10, 9), facecolors='tab:blue')
 ax.set_ylim(5, 25)
 ax.set_xlabel('seconds since start')
 ax.set_yticks([15, 25])
+ax.set_xticks([i for i in range(0, bars[-1][1], int(1e6))])
 
 ax.grid(True)
-
 
 f.close()
 plt.show()
